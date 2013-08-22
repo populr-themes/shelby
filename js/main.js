756cb7725ab118757e869e6701c58a02a3614f90
Jared A. Scheel <jared@jaredscheel.com>
2013-08-22 14:41:37 -0500

    fixed click overlays

--- js/main.js
+++ js/main.js
@@ -1,20 +1,8 @@
-// var fixColumnHeights = function() {
-//   $('.columnizer-row').each(function(){
-//     var maxHeight = 0;
-//     $(this).find('.asset-inner').each(function(){
-//       $(this).height('auto');
-//       var innerHeight = $(this).innerHeight();
-//       if (innerHeight > maxHeight) {
-//         maxHeight = innerHeight;
-//       }
-//     });
-//     $(this).find('.asset-inner').innerHeight(maxHeight);
-//   });
-// }
-
-// $(document).on('pop-initialized', function(){
-//   $('.columnizer-row .asset').live('initialize', fixColumnHeights);
-//   $('.columnizer-row .asset').live('destroy', function(){
-//     $(this).find('.asset-inner').height('auto');
-//   });
-// });
+$(document).on('pop-initialized', function(){
+  $('.asset-type-imagegroup').live('initialize', function(){
+    $(this).find('.slide').each(function(){
+      var $el = $(this).find('a').addClass('overlay');
+      $el.find('img').insertAfter($el);
+    });
+  });
+});
